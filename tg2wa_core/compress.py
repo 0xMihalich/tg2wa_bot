0243af7094301b00e2ed6f7823e24f12a936c81c
0xMihalich <bayan-mobile@list.ru>
2022-09-18 21:50:36 +1000

    убрал лишнее

--- tg2wa_core/compress.py
+++ tg2wa_core/compress.py
@@ -1,15 +1,12 @@
 from PIL import Image
 from asyncio import get_running_loop
 from functools import partial
-from .fix_size import fix_size
 
 
 def compressor(frame: str):
     img = Image.open(frame)
-    img_resized = Image.new(mode="RGBA", size=(512, 512))
-    img_resized.paste(img, fix_size(img.size))
-    img_resized.save(frame, "WEBP", optimize=True, quality=60)
-    del img, img_resized
+    img.save(frame, "WEBP", optimize=True, quality=60)
+    del img
 
 
 async def compress(frame: str):
